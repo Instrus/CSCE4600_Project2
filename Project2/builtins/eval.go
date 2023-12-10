5e7568d62a1f6bd2a4cc1df3b16caabf865bd01e
Instrus <Zegrux@gmail.com>
2023-12-10 16:28:32 -0600

    Final

--- Project2/builtins/eval.go
+++ Project2/builtins/eval.go
@@ -6,17 +6,12 @@ import (
 )
 
 func Eval(args ...string) error {
-	// Execute the command using the exec.Command function
 	cmd := exec.Command(args[0], args[1:]...)
 
-	// Capture the output of the command
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error executing command: %v", err)
 	}
-
-	// Print the output of the command (optional)
 	fmt.Printf("Command output: %s\n", output)
-
 	return nil
 }
